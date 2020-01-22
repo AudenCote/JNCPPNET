c3e30fc29f711dbb2ea471b821bbd553a1ac78bf
AudenCote <audenemil@gmail.com>
2020-01-21 19:49:45 -0500

    worked on matrix header and added note.txt file

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
-
-#define LOG(x) std::cout << x << std::endl;
+#include "utils.h"
 
 
 class Matrix {
@@ -11,27 +10,34 @@ public:
 
     Matrix (std::vector<int>& params) : dims(params.size()), shape(params) { }
 
-    std::vector<int>& makeShape(std::vector<int>& ls){
+    std::vector<std::vector<int>> makeShape(std::vector<int> ls){
 
-        for(int i = 0; i < shape.size() - 1; ++i)
-            std::vector<int> main;
-            for(int j = 0; j < ls.size() - 1; ++j)
-                if(j % shape[i] == 0)
-                    main.push_back(ls[j:j+shape[i]])
-            ls = main; //ASSIGN THIS AS REFERENCE SO THAT VECTOR IS NOT BEING COPIED?
+        std::vector<std::vector<int>> main;
+
+        for(int i = 0; i < shape.size(); ++i){
+            main = {};
+            for(int j = 0; j < ls.size(); ++j){
+                if(j % shape[i] == 0){
+                    main.push_back(get_sec(ls, j, j+shape[i] - 1));
+                }
+            }
+            std::cout << "main: ";
+            print_vec(main);
+            ls = main; 
+        }
+
+        return main;
 
     }
 };
 
 int main(){
 
-    std::vector<int> params = {2, 2, 3, 4};
+    std::vector<int> params = {2, 2};
 
     Matrix mat(params);
 
-    mat.makeShape();
-
-    LOG(mat.dims);
+    print_vec(mat.makeShape({1, 2, 3, 4}));
 
     return 0;
 }
---
+++ lib/utils.h
@@ -0,0 +1,16 @@
+std::vector<int> get_sec(std::vector<int>& v, int start, int end){
+    std::vector<int> vn((end-start)+1);
+
+    for(int i = 0; i < (end-start)+1; ++i){
+        vn[i] = v[i + start];
+    }
+
+    return vn;
+}
+
+void print_vec(std::vector<std::vector<int>> const input){
+    for(int i = 0; i < input.size(); ++i){
+        std::cout << input[i] << ' ';
+    }
+    std::cout << std::endl;
+}
